8f0e7eb302c3bad3434fcef0aef0f8824c2012c3
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-23 01:00:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,24 @@
 
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  def organizer(pigeon_data)
+      pigeon_hash = {}
+
+      pigeon_data.each do |outer_h, inner_h|
+        inner_h.each do |key, pigeons|
+          pigeons.each do |pigeon|
+            
+            if pigeon_hash.include?("#{pigeon}") == false 
+              pigeon_hash[pigeon] = {}
+            end
+
+            if pigeon_hash[pigeon][outer_h] == nil 
+              pigeon_hash[pigeon][outer_h] = []
+              end
+                    
+           pigeon_hash[pigeon][outer_h] << key.to_s
+          end
+        end
+      end
+  pigeon_hash
+  end
 end
