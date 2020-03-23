ce480288538cf4ad163478450a434a80fdc774cd
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-23 01:00:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,15 +6,15 @@ def nyc_pigeon_organizer(data)
       pigeon_data.each do |outer_h, inner_h|
         inner_h.each do |key, pigeons|
           pigeons.each do |pigeon|
-            
-            if pigeon_hash.include?("#{pigeon}") == false 
+
+            if pigeon_hash.include?("#{pigeon}") == false
               pigeon_hash[pigeon] = {}
             end
 
-            if pigeon_hash[pigeon][outer_h] == nil 
+            if pigeon_hash[pigeon][outer_h] == nil
               pigeon_hash[pigeon][outer_h] = []
               end
-                    
+
            pigeon_hash[pigeon][outer_h] << key.to_s
           end
         end
